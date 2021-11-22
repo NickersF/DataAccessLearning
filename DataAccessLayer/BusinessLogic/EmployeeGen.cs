ad11edcdf3dfc00e64ce822fcad4ad7135bf8398
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2021-11-21 20:00:16 -0800

    Add project files.

---
+++ DataAccessLayer/BusinessLogic/EmployeeGen.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Models;
+using DataAccessLayer.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.BusinessLogic
+{
+    public static class EmployeeGen
+    {
+        public static List<EmployeeModel> LoadAllEmployees()
+        {
+            string sql = "dbo.Select_All_Employees";
+
+            return SqlDataAccess.LoadData<EmployeeModel>(sql);
+        }
+
+        public static 
+    }
+}
---
+++ MVCAppDataLearning/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using MVCAppDataLearning.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static DataAccessLayer.BusinessLogic.EmployeeGen;
+
+namespace MVCAppDataLearning.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewEmployees()
+        {
+            var data = LoadAllEmployees();
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+
+            foreach (var row in data)
+            {
+                employees.Add(new EmployeeModel
+                {
+                    Employee_Id = row.Employee_Id,
+                    FirstName = row.FirstName,
+                    LastName = row.LastName,
+                    EmailAddress = row.EmailAddress
+                });
+            }
+
+            return View(employees);
+        }
+    }
+}
