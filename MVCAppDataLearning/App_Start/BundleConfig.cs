57aacd465fabc7c7b825fe452278e8b917973fab
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2021-11-24 23:15:03 -0800

    Updated bootstrap

--- MVCAppDataLearning/App_Start/BundleConfig.cs
+++ MVCAppDataLearning/App_Start/BundleConfig.cs
@@ -28,7 +28,7 @@ namespace MVCAppDataLearning
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
